ba5f0ba823ba99e401355ce15835d5932575506f
mahorabm@gmail.com <mahorabm@gmail.com>
2023-11-04 14:09:33 +0600

    V2.0.0

--- src/schemas/ban.js
+++
@@ -1,9 +0,0 @@
-const { model, Schema } = require('mongoose');
-
-let bans = new Schema({
-    guildId: String,
-    userId: String,
-    reason: String
-});
-
-module.exports = model('bans', bans);
