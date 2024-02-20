3088cd639d8b614f9f797f57b0c590f5f5373156
finneh4249 <ethancornwill@outlook.com>
2024-02-20 20:42:10 +1100

    significant refactoring

--- Structures/Functions/EventLoader.js
+++ Structures/Functions/EventLoader.js
@@ -7,45 +7,44 @@ const fs = require('fs');
 async function loadEvents(client) {
 
   await client.events.clear()
-  
+
   let Loaded = 0;
   let Failed = 0;
 
-
-  const folders = fs.readdirSync(`./Events`)
+  const folders = fs.readdirSync(`./Events`);
 
   for (const folder of folders) {
-
-    const files = fs.readdirSync(`./Events/${folder}`).filter((file) => file.endsWith(".js"));
-
-    for (const file of files) {
+    await Promise.all(fs.readdirSync(`./Events/${folder}`).filter((file) => file.endsWith(".js")).map(async (file) => {
       const event = require(`../../Events/${folder}/${file}`);
       
-      if (!event.name) return Failed++
-
-      const execute = (...args) => event.execute(...args, client);
-      client.events.set(event.name, execute);
-
-      if (event.rest) {
-        if (event.once) {
-          client.rest.once(event.name, execute);
-        } else {
-          client.rest.on(event.name, execute);
-        }
+      if (!event.name) {
+        Failed++;
       } else {
-        if (event.once) client.once(event.name, execute);
-        else
-          client.on(event.name, execute);
+        const execute = (...args) => event.execute(...args, client);
+        client.events.set(event.name, execute);
+
+        if (event.rest) {
+          if (event.once) {
+            client.rest.once(event.name, execute);
+          } else {
+            client.rest.on(event.name, execute);
+          }
+        } else {
+          if (event.once) {
+            client.once(event.name, execute);
+          } else {
+            client.on(event.name, execute);
+          }
+        }
+
+        Loaded++;
       }
-
-      Loaded++
-    }
-
+    }));
   }
 
   if (Loaded !== 0) console.log(`Loaded ${Loaded} events`);
   if (Failed !== 0) console.log(`Failed to load ${Failed} events`);
-  
+
 }
 
 module.exports = { loadEvents };
--- Systems/CountFilter.js
+++ Systems/CountFilter.js
@@ -12,106 +12,38 @@ function filterContent(message, guildData, userData) {
 
   const messageContent = message.content.toLowerCase();
   let output = 'seven';
+  let number;
   
-  const alphabet = `a` || `b` || `c` || `d` || `e` || `f` || `g` || `h` || `i` || `j` || `k` || `l` || `m` || `n` || `o` || `p` || `q` || `r` || `s` || `t` || `u` || `v` || `w` || `x` || `y` || `z`;
-
-  if (messageContent.includes(alphabet)) {
-    output = 'one'
-    return output;
-  } else {
-
-    if (guildData.math == false) {
-      const mathCal = math.evaluate(`${messageContent}`);
-      const number = parseInt(mathCal);
-      
-      if (isNaN(number)) {
-        output = 'one';
-        return output;
-      } else if (message.author.id == guildData.lastUser && userData.saves !== 0) {
-        output = 'two';
-        return output;
-      
-      } else if (message.author.id == guildData.lastUser && guildData.saves !== 0) {
-        
-        output = 'three';
-        return output;
-      
-      } else if (message.author.id == guildData.lastUser) {
-        
-        output = 'four';
-        return output;
-      
-      } else if (guildData.score == 0 && number !== guildData.score + 1) {
-        
-        output = 'five';
-        return output;
-      
-      } else if (number !== guildData.score + 1 && userData.saves !== 0) {
-        
-        output = 'two';
-        return output;
-        
-      } else if (number !== guildData.score + 1 && guildData.saves !== 0) {
-        
-        output = 'three';
-        return output;
-      
-      } else if (number !== guildData.score + 1) {
-        
-        output = 'six';
-        return output;
-      
-      } else {
-        
-        output = 'seven';
-        return output;
-      }
-    } else {
-      const number = parseInt(messageContent);
-      if (isNaN(number)) {
-        output = 'one';
-        return output;
-      } else if (message.author.id == guildData.lastUser && userData.saves !== 0) {
-        output = 'two';
-        return output;
-      
-      } else if (message.author.id == guildData.lastUser && guildData.saves !== 0) {
-      
-        output = 'three';
-        return output;
-      
-      } else if (message.author.id == guildData.lastUser) {
-      
-        output = 'four';
-        return output;
-      
-      } else if (guildData.score == 0 && number !== guildData.score + 1) {
-      
-        output = 'five';
-        return output;
-      
-      } else if (number !== guildData.score + 1 && userData.saves !== 0) {
-      
-        output = 'two';
-        return output;
-      
-      } else if (number !== guildData.score + 1 && guildData.saves !== 0) {
-      
-        output = 'three';
-        return output;
-      
-      } else if (number !== guildData.score + 1) {
-      
-        output = 'six';
-        return output;
-      
-      } else {
-      
-        output = 'seven';
-        return output;
-      }
-    }
+  if (!isNaN(messageContent)) {
+    number = parseInt(messageContent);
   }
+
+  if (guildData.math == false && !isNaN(number)) {
+    const mathCal = math.evaluate(`${messageContent}`);
+    number = parseInt(mathCal);
+  }
+  
+  if (message.author.id == guildData.lastUser) {
+    if (userData.saves !== 0) {
+      output = 'two';
+    } else if (guildData.saves !== 0) {
+      output = 'three';
+    } else {
+      output = 'four';
+    }
+  } else if (guildData.score == 0 && number !== guildData.score + 1) {
+    output = 'five';
+  } else if (number !== guildData.score + 1 && userData.saves !== 0) {
+    output = 'two';
+  } else if (number !== guildData.score + 1 && guildData.saves !== 0) {
+    output = 'three';
+  } else if (number !== guildData.score + 1) {
+    output = 'six';
+  } else {
+    output = 'one';
+  }
+  
+  return output;
 }
 
 module.exports = { filterContent };
